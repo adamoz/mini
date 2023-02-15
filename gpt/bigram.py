0503eb1ff37ad9c9a2d640aac63cbf2110323880
Adam Kolar <adakolar@gmail.com>
2023-02-16 00:42:52 +0100

    feature: self attention exploration

--- gpt/bigram.py
+++ gpt/bigram.py
@@ -4,6 +4,7 @@ from torch.nn import functional as F
 
 block_size = 8
 batch_size = 32
+n_embd = 32
 max_iters = 3000
 eval_iters = 200
 eval_interval = 300
@@ -76,7 +77,46 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel(vocab_size)
+class BigramLanguageModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+    def forward(self, idx, targets=None):
+        assert torch.all(idx >= 0), "idx tensor contains negative indices"
+        assert torch.all(idx < self.token_embedding_table.num_embeddings), "idx tensor contains out-of-range indices"
+        
+        B, T = idx.shape
+        # pos_emb = self.position_embedding_table(
+        #    torch.arange(T, device=device))  # Timestamp n_embd
+        tok_emb = self.token_embedding_table(idx)  # Batch Timestamp n_embd
+        # + pos_emb)  # Batch Timestamp vocab_size
+        logits = self.lm_head(tok_emb)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, _ = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=1)
+            assert torch.all(probs >= 0), "probs tensor contains negative values"
+            assert torch.allclose(torch.sum(probs, dim=1), torch.ones(probs.shape[0], device=device)), "probs tensor does not sum to 1"
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+model = BigramLanguageModelV2()
 model = model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
