59824421d8fe719941c14629e63094c5d2867029
Adam Kolar <adakolar@gmail.com>
2023-02-17 10:13:40 +0100

    feature: remormatting

--- gpt/bigram.py
+++ gpt/bigram.py
@@ -57,6 +57,8 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
 
     def forward(self, idx, targets=None):
+        # idx and targets: Batch Timestamp
+
         logits = self.token_embedding_table(idx)  # Batch Timestamp Channels
         if targets is None:
             loss = None
@@ -70,7 +72,7 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
             logits, _ = self(idx)
-            logits = logits[:, -1, :]
+            logits = logits[:, -1, :] # Batch vocab_size
             probs = F.softmax(logits, dim=1)
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1)
@@ -89,8 +91,7 @@ class BigramLanguageModelV2(nn.Module):
         assert torch.all(idx < self.token_embedding_table.num_embeddings), "idx tensor contains out-of-range indices"
         
         B, T = idx.shape
-        # pos_emb = self.position_embedding_table(
-        #    torch.arange(T, device=device))  # Timestamp n_embd
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # Timestamp n_embd
         tok_emb = self.token_embedding_table(idx)  # Batch Timestamp n_embd
         # + pos_emb)  # Batch Timestamp vocab_size
         logits = self.lm_head(tok_emb)
