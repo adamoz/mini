c647476dcd905050baf56b2963ab4408944f46e1
Adam Kolar <akolar@mindmed.com>
2023-03-07 15:21:17 +0000

    feature: multihead attention in one matrix

--- transformer/decoder.py
+++ transformer/decoder.py
@@ -1,21 +1,32 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 torch.manual_seed(1337)
+
+
+# @torch.jit.scrip
+def new_gelu(x):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
+    Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
+    """
+    return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
 
 
 class FeedForward(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(config.n_embd, 4 * config.n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * config.n_embd, config.n_embd),
-            nn.Dropout(config.dropout)
-        )
+        self.l1 = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.l2 = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
-        return self.net(x)
+        x = self.l1(x)
+        x = new_gelu(x)
+        x = self.l2(x)
+        x = self.dropout(x)
+        return x
 
 
 class Head(nn.Module):
@@ -60,10 +71,52 @@ class MultiHeadAttention(nn.Module):
         return self.dropout(self.proj(out))
 
 
+class MultiHeadAttentionV2(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_heads == 0
+        # key, query, value projections for all heads, but in a batch
+        # n_embed is split into n_heads
+        # 3 n_embd covers query value and key (for all heads)
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=False)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_heads = config.n_heads
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        # causal mask to ensure that attention is only applied to the left in the input sequence
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_heads, C // self.n_heads).transpose(1, 2) # (B, nh, T, hs)
+
+        # manual implementation of attention
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+    
+
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.sa_head = MultiHeadAttention(config)
+        self.sa_head = MultiHeadAttentionV2(config)
         self.ffwd = FeedForward(config)
         # B T behaves as a batch in normalization layer
         self.ln1 = nn.LayerNorm(config.n_embd)
